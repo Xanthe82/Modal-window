94b6c95e4b5eb9c7af77b79eb1c01d9aa5f2f5ec
Xanthe Sharp <xsharp@virginmedia.com>
2021-06-24 10:58:52 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden'); // don't use dot for class after remove
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal); // don't use () after function name
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
